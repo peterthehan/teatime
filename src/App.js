418e544868078eee731a3504654fdc320eb5b38e
Peter Han <peter.han.9493@gmail.com>
2025-09-13 01:49:52 -0700

    Calculate precise calendar duration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles.css";
 
 const TITLE_URL_PARAM = "title";
@@ -25,24 +25,43 @@ function getDateFromUrl() {
   return date;
 }
 
-function calculateTimeValues(ms) {
-  const units = {
-    year: 1000 * 60 * 60 * 24 * 365,
-    month: 1000 * 60 * 60 * 24 * 30,
-    week: 1000 * 60 * 60 * 24 * 7,
-    day: 1000 * 60 * 60 * 24,
-    hour: 1000 * 60 * 60,
-    minute: 1000 * 60,
-    second: 1000,
-  };
-
-  const result = {};
-  for (const [unit, value] of Object.entries(units)) {
-    result[unit] = Math.floor(ms / value);
-    ms %= value;
+function calculateTimeValues(startDate, endDate) {
+  if (startDate > endDate) {
+    [startDate, endDate] = [endDate, startDate];
   }
 
-  return result;
+  let temp = new Date(startDate);
+
+  function addUnit(unit, value) {
+    let test = new Date(temp);
+    if (unit === "year") test.setFullYear(test.getFullYear() + value);
+    if (unit === "month") test.setMonth(test.getMonth() + value);
+    if (unit === "day") test.setDate(test.getDate() + value);
+    if (unit === "hour") test.setHours(test.getHours() + value);
+    if (unit === "minute") test.setMinutes(test.getMinutes() + value);
+    if (unit === "second") test.setSeconds(test.getSeconds() + value);
+    return test <= endDate ? test : null;
+  }
+
+  function countUnit(unit) {
+    let value = 0;
+    while (addUnit(unit, value + 1)) {
+      ++value;
+    }
+    temp = addUnit(unit, value) ?? temp;
+    return value;
+  }
+
+  const year = countUnit("year");
+  const month = countUnit("month");
+  let day = countUnit("day");
+  const week = Math.floor(day / 7);
+  day %= 7;
+  const hour = countUnit("hour");
+  const minute = countUnit("minute");
+  const second = countUnit("second");
+
+  return { year, month, week, day, hour, minute, second };
 }
 
 function filterLeadingZeroes(timeValues) {
@@ -68,7 +87,7 @@ function App() {
   }, []);
 
   const timeDifference = Math.abs(now - date);
-  const timeValues = calculateTimeValues(timeDifference);
+  const timeValues = calculateTimeValues(date, now);
 
   const displayTimeValues = filterLeadingZeroes(timeValues).map(
     ([unit, value]) => ({
